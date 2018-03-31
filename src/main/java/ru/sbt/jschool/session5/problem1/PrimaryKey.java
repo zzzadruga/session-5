c1976687e577eec2d69c46daa0836e8e2389a837
Nikolay Izhikov <NIzhikov.dev@gmail.com>
2018-03-31 08:16:03 +0300

    Task updated

--- src/main/java/ru/sbt/jschool/session5/problem1/PrimaryKey.java
+++ src/main/java/ru/sbt/jschool/session5/problem1/PrimaryKey.java
@@ -1,7 +1,14 @@
 package ru.sbt.jschool.session5.problem1;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface PrimaryKey {
     String name() default "";
 }
--- src/main/java/ru/sbt/jschool/session5/problem1/Table.java
+++ src/main/java/ru/sbt/jschool/session5/problem1/Table.java
@@ -1,8 +1,15 @@
 package ru.sbt.jschool.session5.problem1;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author NIzhikov
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Table {
     String name() default "";
 }
