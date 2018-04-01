3e157e5b16807a546f3f6769a15e04c12bf6e3b9
zzzadruga <zzzadruga@gmail.com>
2018-04-01 14:38:55 +0300

    implement "select" method

--- src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
+++ src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
@@ -1,5 +1,7 @@
 package ru.sbt.jschool.session5.problem1;
 
+import java.lang.reflect.Field;
+
 /**
  */
 public class SQLGenerator {
@@ -16,6 +18,22 @@ public class SQLGenerator {
     }
 
     public <T> String select(Class<T> clazz) {
-        return null;
+        StringBuilder query = new StringBuilder();
+        StringBuilder primaryKey = new StringBuilder();
+        for(Field field : clazz.getDeclaredFields()){
+            if (field.getAnnotation(Column.class) != null){
+                query.append(query.length() == 0 ? "SELECT " : ", ")
+                     .append((field.getAnnotation(Column.class).name().isEmpty() ? field.getName() : field.getAnnotation(Column.class).name()).toLowerCase());
+            }
+            else if (field.getAnnotation(PrimaryKey.class) != null) {
+                    primaryKey.append(primaryKey.length() == 0 ? "" : " AND ")
+                              .append(field.getName().toLowerCase()).append(" = ?");
+            }
+        }
+        query.append(" FROM ")
+             .append(clazz.getAnnotation(Table.class) != null ? clazz.getAnnotation(Table.class).name() : clazz.getSimpleName())
+             .append(" WHERE ")
+             .append(primaryKey);
+        return query.toString();
     }
 }
