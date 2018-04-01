71497dd45e8f86cef3b469ce5776b8fdf064a691
zzzadruga <zzzadruga@gmail.com>
2018-04-01 19:34:07 +0300

    refactor all methods

--- src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
+++ src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
@@ -1,5 +1,7 @@
 package ru.sbt.jschool.session5.problem1;
 
+import org.springframework.util.StringUtils;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -10,87 +12,88 @@ import java.util.*;
 public class SQLGenerator {
     private static final String SEPARATOR_COMMA = ", ";
     private static final String SEPARATOR_AND = " AND ";
+
     public <T> String insert(Class<T> clazz) {
         StringBuilder query = new StringBuilder();
-        StringBuilder values = new StringBuilder();
-        query.append("INSERT INTO ")
-                .append(getTableName(clazz));
-        getFields(clazz).forEach((k, v) -> {
-            if (!v.equals(FieldsAnnotation.NONE)){
-                query.append(values.length() == 0 ? "(" : ", ")
-                        .append(k);
-                values.append(values.length() == 0 ? "?" : ", ?");
-            }
-        });
-        query.append(") VALUES (")
-                .append(values)
-                .append(")");
-        System.out.println(getFields(clazz));
-        return query.toString();
+        Set<String> columnSet = getColumnSet(getFields(clazz), FieldsAnnotation.COLUMN_NAME, FieldsAnnotation.PRIMARY_KEY);
+        return query
+                .append("INSERT INTO ")
+                .append(getTableName(clazz))
+                .append("(")
+                .append(getFormattedColumnList(columnSet, false, SEPARATOR_COMMA))
+                .append(") VALUES (")
+                .append(String.join(SEPARATOR_COMMA, Collections.nCopies(columnSet.size(), "?")))
+                .append(")")
+                .toString();
     }
 
     public <T> String update(Class<T> clazz) {
         StringBuilder query = new StringBuilder();
-        Map fields = getFields(clazz);
-        query.append("UPDATE ")
-             .append(getTableName(clazz))
-             .append(" SET ")
-             .append(getFormattedColumnList(fields, FieldsAnnotation.COLUMN_NAME, true, SEPARATOR_COMMA))
-             .append(" WHERE ")
-             .append(getFormattedColumnList(fields, FieldsAnnotation.PRIMARY_KEY, true, SEPARATOR_AND));
-        return query.toString();
+        Map<String, FieldsAnnotation> fields = getFields(clazz);
+        return query.append("UPDATE ")
+                .append(getTableName(clazz))
+                .append(" SET ")
+                .append(getFormattedColumnList(fields, true, SEPARATOR_COMMA, FieldsAnnotation.COLUMN_NAME))
+                .append(" WHERE ")
+                .append(getFormattedColumnList(fields, true, SEPARATOR_AND, FieldsAnnotation.PRIMARY_KEY))
+                .toString();
     }
 
     public <T> String delete(Class<T> clazz) {
         StringBuilder query = new StringBuilder();
-        query.append("DELETE FROM ")
-             .append(getTableName(clazz))
-             .append(" WHERE ")
-             .append(getFormattedColumnList(getFields(clazz), FieldsAnnotation.PRIMARY_KEY, true, SEPARATOR_AND));
-        return query.toString();
+        return query.append("DELETE FROM ")
+                .append(getTableName(clazz))
+                .append(" WHERE ")
+                .append(getFormattedColumnList(getFields(clazz), true, SEPARATOR_AND, FieldsAnnotation.PRIMARY_KEY))
+                .toString();
     }
 
     public <T> String select(Class<T> clazz) {
         StringBuilder query = new StringBuilder();
-        Map fields = getFields(clazz);
-        query.append("SELECT ")
-             .append(getFormattedColumnList(fields, FieldsAnnotation.COLUMN_NAME, false, SEPARATOR_COMMA))
-             .append(" FROM ")
-             .append(getTableName(clazz))
-             .append(" WHERE ")
-             .append(getFormattedColumnList(fields, FieldsAnnotation.PRIMARY_KEY, true, SEPARATOR_AND));
-        return query.toString();
-    }
-    private String getFormattedColumnList(Map<String, FieldsAnnotation> fields, FieldsAnnotation annotation, boolean isArguments, String separator){
-        StringBuilder listOfColumns = new StringBuilder();
-        String argument = isArguments ? " = ?" : "";
-        fields.forEach((k, v) -> {
-            if (v.equals(annotation)) {
-                listOfColumns.append(listOfColumns.length() == 0 ? "" : argument + separator);
-                listOfColumns.append(k);
-            }
-        });
-        listOfColumns.append(argument);
-        return listOfColumns.toString();
+        Map<String, FieldsAnnotation> fields = getFields(clazz);
+        return query.append("SELECT ")
+                .append(getFormattedColumnList(fields, false, SEPARATOR_COMMA, FieldsAnnotation.COLUMN_NAME))
+                .append(" FROM ")
+                .append(getTableName(clazz))
+                .append(" WHERE ")
+                .append(getFormattedColumnList(fields, true, SEPARATOR_AND, FieldsAnnotation.PRIMARY_KEY))
+                .toString();
     }
 
-    private <T> Map<String, FieldsAnnotation> getFields(Class<T> clazz){
+    private String getFormattedColumnList(Map<String, FieldsAnnotation> fields, boolean isArguments, String separator, FieldsAnnotation... annotations) {
+        return StringUtils.collectionToDelimitedString(getColumnSet(fields, annotations), separator, "", isArguments ? " = ?" : "");
+    }
+
+    private String getFormattedColumnList(Set<String> fields, boolean isArguments, String separator) {
+        return StringUtils.collectionToDelimitedString(fields, separator, "", isArguments ? " = ?" : "");
+    }
+
+    private Set<String> getColumnSet(Map<String, FieldsAnnotation> fields, FieldsAnnotation... annotations) {
+        Set<FieldsAnnotation> annotationSet = new LinkedHashSet<>(Arrays.asList(annotations));
+        Set<String> columnSet = new LinkedHashSet<>();
+        fields.forEach((k, v) -> {
+            if (annotationSet.contains(v)) {
+                columnSet.add(k);
+            }
+        });
+        return columnSet;
+    }
+
+    private <T> Map<String, FieldsAnnotation> getFields(Class<T> clazz) {
         Map<String, FieldsAnnotation> fields = new LinkedHashMap<>();
-        for(Field field : clazz.getDeclaredFields()){
-            if (field.getAnnotation(Column.class) != null){
+        for (Field field : clazz.getDeclaredFields()) {
+            if (field.getAnnotation(Column.class) != null) {
                 fields.put(getColumnName(Column.class, field), FieldsAnnotation.COLUMN_NAME);
-            }
-            else if (field.getAnnotation(PrimaryKey.class) != null) {
+            } else if (field.getAnnotation(PrimaryKey.class) != null) {
                 fields.put(getColumnName(PrimaryKey.class, field), FieldsAnnotation.PRIMARY_KEY);
-            }
-            else{
+            } else {
                 fields.put(field.getName(), FieldsAnnotation.NONE);
             }
         }
         return fields;
     }
 
-    private <T extends Annotation> String getColumnName(Class<T> clazz, Field field){
+    private <T extends Annotation> String getColumnName(Class<T> clazz, Field field) {
         String methodValue = field.getName().toLowerCase();
         try {
             methodValue = String.valueOf(clazz.getMethod("name").invoke(field.getAnnotation(clazz)));
@@ -101,8 +104,7 @@ public class SQLGenerator {
         return methodValue;
     }
 
-    private <T> String getTableName(Class<T> clazz){
+    private <T> String getTableName(Class<T> clazz) {
         return clazz.getAnnotation(Table.class) != null ? clazz.getAnnotation(Table.class).name() : clazz.getSimpleName();
     }
-
 }
